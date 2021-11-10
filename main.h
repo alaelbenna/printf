17381329814675630f66a448837b05a41c43a7a6
alaelbenna <elbenna.ala@outlook.com>
2021-11-10 12:14:34 -0800

    implementation of printf

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <limits.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+//Prototype of the functions
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+
+#endif // MAIN_H_INCLUDED
+
