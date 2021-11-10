a12b07db086cf126cff789e2f956ad1ceeee3376
alaelbenna <elbenna.ala@outlook.com>
2021-11-10 12:42:25 -0800

    betty update

--- _printf.c
+++ _printf.c
@@ -1,60 +1,50 @@
 #include "main.h"
 int _putchar(char c)
 {
-
-    return (write(1,&c,1));
-
+	return (write(1,&c,1));
 }
 
 int _printf(const char *format, ...)
 {
-
-    if( !format || (format[0] == '%' && format[1] == '\0') )
-    {
-        return -1;
-    }
-    va_list arg;
-    va_start(arg,format);
-    int i = 0;
-    int nbPrinted =  0;
-    char * currentLetter ;
-    while( *(format + i)  != '\0' )
-    {
-
-        if( *(format + i) != '%' )
-        {
-            _putchar(*(format + i));
-            nbPrinted++;
-        }
-        else
-        {
-            switch (*(format + i + 1))
-            {
-            case 'c':
-                _putchar( (char) va_arg(arg,int));
-                nbPrinted++;
-
-                break;
-            case 's':
-                currentLetter =  va_arg(arg,char *);
-                while ( (*currentLetter) != '\0')
-                {
-                    _putchar(*currentLetter);
-                    nbPrinted++;
-
-                    currentLetter++;
-                }
-                break;
-            }
-            i++;
-        }
-        i++;
-
-    }
-
-
-    va_end(arg);
-    return nbPrinted;
-
+	if( !format || (format[0] == '%' && format[1] == '\0') )
+	{
+		return -1;
+	}
+	va_list arg;
+	va_start(arg,format);
+	int i = 0;
+	int nbPrinted =  0;
+	char * currentLetter ;
+	while( *(format + i)  != '\0' )
+	{
+	if( *(format + i) != '%' )
+	{
+		_putchar(*(format + i));
+		nbPrinted++;
+	}
+	else
+	{
+	switch (*(format + i + 1))
+	{
+		case 'c':
+			_putchar( (char) va_arg(arg,int));
+			nbPrinted++;
+		break;
+		case 's':
+			currentLetter =  va_arg(arg,char *);
+			while ( (*currentLetter) != '\0')
+			{
+				_putchar(*currentLetter);
+				nbPrinted++;
+				currentLetter++;
+			}
+			break;
+	}
+	i++;		
+	}
+	i++;
+	}
+	va_end(arg);
+	return nbPrinted;
 }
 
